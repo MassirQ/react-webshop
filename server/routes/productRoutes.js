ffa5917f81b26f071062226449a74d9ec142d851
MassirQ <aqauomi@gmail.com>
2024-11-05 19:01:07 +0100

    Pagination is added

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,22 +1,29 @@
 import express from "express";
 import Product from "../models/Product.js";
 
-
-const productRoutes = express.Router()
+const productRoutes = express.Router();
 
 const getProducts = async (req, res) => {
+  const page = parseInt(req.params.page);
+  const perPage = parseInt(req.params.perPage);
 
-    const products = await Product.find({});
+  const products = await Product.find({});
 
+  if (page && perPage) {
+    const totalPages = Math.ceil(products.length / perPage);
+    const startIndex = (page - 1) * perPage;
+    const endIndex = startIndex + perPage;
+    const paginatedProducts = products.slice(startIndex, endIndex);
     res.json({
-        products,
-        pagination: {},
-
+      products: paginatedProducts,
+      pagination: { currentPage: page, totalPages },
     });
-
-
+  } else {
+    res.json({ products, pagination: {} });
+  }
 };
 
-productRoutes.route('/').get(getProducts);
+productRoutes.route("/:page/:perPage").get(getProducts);
+productRoutes.route("/").get(getProducts);
 
 export default productRoutes;
